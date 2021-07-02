d5dfc346e8aa0ffab5cfa88dc3274f9572523c5a
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-01 22:21:22 -0400

    Bugfixing

--- upload_music.php
+++ upload_music.php
@@ -2,23 +2,23 @@
 
 //TODO: Prevent special characters from being uploaded
 $target_dir = "/tmp/uploads/";
-    $target_filename = basename($_FILES["musicFile"]["name"]);
-    echo "Basename: " . $_FILES["musicFile"]["name"]."<br>";
-    echo "Target dir: " . $target_dir."<br>";
-    echo "Target filename: " . $target_filename."<br>";
+$target_filename = basename($_FILES["musicFile"]["name"]);
+echo "Basename: " . $_FILES["musicFile"]["name"]."<br>";
+echo "Target dir: " . $target_dir."<br>";
+echo "Target filename: " . $target_filename."<br>";
 
-    $target_file = $target_dir.$target_filename;
-    echo "Target file: " . $target_file."<br>";
-    if(is_writable($target_file)) {
-        echo "Writeable!<br>";
-    } else {
-        echo "Not writeable!<br>";
-    }
-    echo $_FILES["musicFile"]["name"]."<br>";
-    //echo "<script type='text/javascript'> alert('".$target_file."');</script>";
+$target_file = $target_dir.$target_filename;
+echo "Target file: " . $target_file."<br>";
+if(is_writable($target_dir)) {
+    echo "Writeable!<br>";
+} else {
+    echo "Not writeable!<br>";
+}
+echo $_FILES["musicFile"]["name"]."<br>";
+//echo "<script type='text/javascript'> alert('".$target_file."');</script>";
 
-    $uploadOK = 1;
-    $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+$uploadOK = 1;
+$fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 if(isset($_POST['submit'])){
     
     //check if file already exitsts

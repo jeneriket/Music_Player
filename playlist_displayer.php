1a856993ce6aea06cbcd19cdcb6dfd6f6257e379
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-02 14:27:43 -0400

    Delete Button

--- playlist_displayer.php
+++ playlist_displayer.php
@@ -1,14 +1,17 @@
 <?php
 
 $musicFiles = scandir("/var/www/html/uploads");
-
 echo "<div name='playlist' key='playlist'>";
 foreach($musicFiles as $value)
 {
     if($value == "." || $value == "..")
         continue;
 
-    echo nl2br($value."<br><audio controls><source src='/uploads/".strval($value)."'></audio><button onclick='alert(\"".$value."\");'>Delete?</button><br>");
+    echo nl2br($value."<br><audio controls><source src='/uploads/".strval($value)."'></audio><button onclick='$.ajax({
+        url: \'delete_song.php\', 
+        success: function(data) { 
+            alert('Deleted.');}
+        })'>Delete?</button><br>");
 
 }
 echo "</div>";

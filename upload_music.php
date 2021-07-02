c2e263955c9f76372b7d9b7e84a73ce35a51a814
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-01 22:45:52 -0400

    Bugfixing

--- upload_music.php
+++ upload_music.php
@@ -1,22 +1,9 @@
 <?php
-ini_set('upload_max_filesize', '40M');
-ini_set('post_max_size', '42M');
 //TODO: Prevent special characters from being uploaded
 $target_dir = "/var/www/html/uploads/";
 $target_filename = basename($_FILES["musicFile"]["name"]);
-echo "Basename: " . $_FILES["musicFile"]["name"]."<br>";
-echo "Target dir: " . $target_dir."<br>";
-echo "Target filename: " . $target_filename."<br>";
 
 $target_file = $target_dir.$target_filename;
-echo "Target file: " . $target_file."<br>";
-if(is_writable($target_dir)) {
-    echo "Writeable!<br>";
-} else {
-    echo "Not writeable!<br>";
-}
-echo $_FILES["musicFile"]["name"]."<br>";
-//echo "<script type='text/javascript'> alert('".$target_file."');</script>";
 
 $uploadOK = 1;
 $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
@@ -26,7 +13,7 @@ if(isset($_POST['submit'])){
     //TODO: Add asking if you want to add duplicate file name
     if(file_exists($target_file))
     {
-        //echo "<script type='text/javascript'> alert('Sorry, the file already exists');</script>";
+        echo "<script type='text/javascript'> alert('Sorry, the file already exists');</script>";
         $uploadOK = 0;
     }
 
@@ -34,18 +21,16 @@ if(isset($_POST['submit'])){
     
     if($uploadOK == 0)
     {
-        //echo "<script type='text/javascript'> alert('Sorry, your file was not uploaded');</script>";
+        echo "<script type='text/javascript'> alert('Sorry, your file was not uploaded');</script>";
     } else {
-        if(copy($_FILES["musicFile"]["tmp_name"], $target_file))
+        if(move_uploaded_file($_FILES["musicFile"]["tmp_name"], $target_file))
         {
-            //echo "<script type='text/javascript'> alert('The file ".htmlspecialchars(basename($_FILES["musicFile"]["name"]))." has been uploaded.');</script>";
+            echo "<script type='text/javascript'> alert('The file ".htmlspecialchars(basename($_FILES["musicFile"]["name"]))." has been uploaded.');</script>";
         } else {
-            //echo "<script type='text/javascript'> alert('Sorry, there was an error uploading your file.');</script>";
+            echo "<script type='text/javascript'> alert('Sorry, there was an error uploading your file.');</script>";
         }
     }
 }
 
-//echo "<script type='text/javascript'>location.replace('http://18.189.30.30');</script>"
-
-echo $target_file;
+echo "<script type='text/javascript'>location.replace('http://18.189.30.30');</script>"
 ?>

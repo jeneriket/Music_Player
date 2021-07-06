25e5eac891e2d6088a5a1a80380c58b60c653e33
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-06 15:24:44 -0400

    Delete button

--- playlist_displayer.php
+++ playlist_displayer.php
@@ -7,12 +7,13 @@ foreach($musicFiles as $value)
     if($value == "." || $value == "..")
         continue;
 
-    echo $value."<div id='".strval($value)."_div'><br><audio controls><source src='/uploads/".strval($value)."'></audio><button onclick=\"$('#status').html('
-    <form action='delete_song.php' method='POST' encType='multipart/form-data'>
-    Delete ".strval($value)."?
-    <input type='submit' value='Yes' name='yes'/>
-    <button onclick='$(\'#status\').html(\'\');'>No</button>
-    ')\">Delete?</button></div>";
+    echo $value."<div id='".strval($value)."_div'><br><audio controls><source src='/uploads/".strval($value)."'></audio>
+    <button onclick=\"$('#status').html(\"
+        <form action='delete_song.php' method='POST' encType='multipart/form-data'>
+        Delete ".strval($value)."?
+        <input type='submit' value='Yes' name='yes'/>
+        <button onclick='$(\'#status\').html(\'\');'>No</button>
+    \")\">Delete?</button></div>";
     /*
     \"$.ajax({
         url: 'delete_song.php', 

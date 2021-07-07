dbe86eb3ba3964817741d2f8b2036ee1518afa71
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-07 11:42:35 -0400

    Delete Button

--- delete_song.php
+++ delete_song.php
@@ -1,7 +1,6 @@
 <?php
-    //echo "<script>alert('/var/www/html/".$_POST['filename']."')</script>";
     if(unlink("/var/www/html/uploads/".$_POST['filename']))
     {
-        echo "/var/www/html/uploads/".$_POST['filename'];
+        //echo "Deleted '".$_POST['filename']."'";
     }
 ?>

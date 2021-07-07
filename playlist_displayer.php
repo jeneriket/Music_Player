762dbf97913e743ffd27b86ee784449932185404
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-07 13:07:11 -0400

    Delete Button

--- playlist_displayer.php
+++ playlist_displayer.php
@@ -1,5 +1,21 @@
 <?php
 
+//todo: delete and add to sql database
+$ids = [];
+
+function GenerateID()
+{
+    $ID = rand();
+    while(in_array($ID, $ids))
+    {
+        $ID = rand();
+    }
+
+    return $ID;
+}
+
+
+
 $musicFiles = scandir("/var/www/html/uploads");
 echo "<div name='playlist' key='playlist'>";
 foreach($musicFiles as $value)
@@ -7,9 +23,10 @@ foreach($musicFiles as $value)
     if($value == "." || $value == "..")
         continue;
 
+    $id = GenerateID();
     //make the div for each playlist button
-    echo "<div id='".strval($value)."_div'>".$value."<br><audio controls><source src='/uploads/".strval($value)."'></audio>
-        <button onclick='confirmDelete(\"".strval($value)."\");'>
+    echo "<div id='".$id."_div'>".$value."<br><audio controls><source src='/uploads/".strval($value)."'></audio>
+        <button onclick='confirmDelete(\"".strval($value)."\", ".$id.");'>
             Delete?
         </button>
     </div>";

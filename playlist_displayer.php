434d7e2f5058d86c4f83fcbd240b62cc32dcf533
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-07 11:59:52 -0400

    Delete Button

--- playlist_displayer.php
+++ playlist_displayer.php
@@ -7,19 +7,12 @@ foreach($musicFiles as $value)
     if($value == "." || $value == "..")
         continue;
 
-    echo $value."<div id='".strval($value)."_div'><br><audio controls><source src='/uploads/".strval($value)."'></audio>
+    //make the div for each playlist button
+    echo "<div id='".strval($value)."_div'>".$value."<br><audio controls><source src='/uploads/".strval($value)."'></audio>
         <button onclick='confirmDelete(\"".strval($value)."\");'>
             Delete?
         </button>
     </div>";
-    /*
-    \"$.ajax({
-        url: 'delete_song.php', 
-        success: function(data) { 
-            alert('Deleted.');
-        }
-    })\">Delete?</button><br></div>";*/
-
 }
 echo "</div>";
 ?>

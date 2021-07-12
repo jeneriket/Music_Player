a173be7c64905aaaaffeb2611ef372a77133275f
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-12 18:19:46 -0400

    Custom Player

--- back_panel.js
+++ back_panel.js
@@ -138,6 +138,7 @@ var BackPanel = function (_React$Component) {
                         { id: id + "_audio", controls: true },
                         React.createElement("source", { src: "/uploads/" + name })
                     ),
+                    React.createElement(MusicInterface, { id: id }),
                     React.createElement(
                         "button",
                         { onClick: function onClick() {

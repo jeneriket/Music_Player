f407afa8b6de538b6a7a586ffe36974977cccaa7
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-13 10:30:55 -0400

    Custom Player

--- upload_music.php
+++ upload_music.php
@@ -41,7 +41,7 @@ if(isset($_POST['submit'])){
             $response = curl_exec($ch);
 
             curl_close($ch);
-            echo(strval($response));
+            echo "SD";
         } else {
             echo "<script type='text/javascript'> alert('Sorry, there was an error uploading your file.');</script>";
         }

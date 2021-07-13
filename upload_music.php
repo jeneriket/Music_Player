f1d43072399d933f7d714733456e7d7de9b09163
Cobra Hubbard <hikiaraite@gmail.com>
2021-07-13 10:43:54 -0400

    Custom Player

--- upload_music.php
+++ upload_music.php
@@ -52,8 +52,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-
-        $sql = "CREATE DATABASE myDB";
+if(true)
+{
+    $sql = "CREATE DATABASE myDB";
+}
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully";
   } else {
